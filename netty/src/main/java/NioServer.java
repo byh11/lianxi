3a84b71c9aafbd29735d332fd50ecc027806079c
byh11 <1152394813@qq.com>
2024-04-18 22:27:12 +0800

    2024-4-18

--- netty/src/main/java/NioServer.java
+++ netty/src/main/java/NioServer.java
@@ -2,6 +2,7 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoop;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -13,23 +14,24 @@ import io.netty.handler.logging.LoggingHandler;
 public class NioServer {
 
     public static void main(String[] args) {
-        NioEventLoopGroup boss=new NioEventLoopGroup();
-        NioEventLoopGroup worker=new NioEventLoopGroup();
-        ServerBootstrap serverBootstrap=new ServerBootstrap();
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        serverBootstrap.option(ChannelOption.SO_RCVBUF,10);
         try {
             serverBootstrap.channel(NioServerSocketChannel.class)
-                    .group(boss,worker)
+                    .group(boss, worker)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel channel) throws Exception {
                             channel.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
                         }
                     });
-            ChannelFuture channelFuture=serverBootstrap.bind(9999).sync();
+            ChannelFuture channelFuture = serverBootstrap.bind(9999).sync();
             channelFuture.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             boss.shutdownGracefully();
             worker.shutdownGracefully();
         }
