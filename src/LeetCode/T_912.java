66bc096af844023c57fcf60b4dcecbb0dd611f75
byh11 <1152394813@qq.com>
2024-05-29 20:48:36 +0800

    2024-05-29

---
+++ src/LeetCode/T_912.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class T_912 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
