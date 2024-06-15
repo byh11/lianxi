1635e370de013546a7af83038277efbc2bb4469b
白云浩 <1152394813@qq.com>
2024-06-15 10:01:10 +0800

    2024-6-15

---
+++ src/LeetCode/T_292.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+public class T_292 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static boolean canWinNim(int n) {
+        return n%4==0;
+    }
+}
---
+++ src/LeetCode/T_670_1.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+public class T_670_1 {
+    public static void main(String[] args) {
+
+        maximumSwap(9973);
+    }
+
+    public static int maximumSwap(int num) {
+        char[] charArray = String.valueOf(num).toCharArray();
+        int n = charArray.length;
+        int maxNum = num;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(charArray, i, j);
+                maxNum = Math.max(maxNum, Integer.parseInt(new String(charArray)));
+                swap(charArray, i, j);
+            }
+        }
+        return maxNum;
+    }
+
+    public static void swap(char[] charArray, int i, int j) {
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+    }
+}
