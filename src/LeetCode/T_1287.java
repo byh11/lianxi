1c9907f9aae4d6cbf41a93de4a07b29d5f932412
白云浩 <1152394813@qq.com>
2024-06-23 20:38:06 +0800

    2024-6-23

--- src/LeetCode/T_1287.java
+++ src/LeetCode/T_1287.java
@@ -1,2 +1,38 @@
-package LeetCode;public class T_1287 {
+package LeetCode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class T_1287 {
+
+    public static void main(String[] args) {
+
+        findSpecialInteger(new int[]{15,15,21,21,32,32,33,33,33,34,35});
+    }
+
+    public static int findSpecialInteger(int[] arr) {
+        if(arr.length==1){
+            return arr[0];
+        }
+        int m=0;
+        int x=0;
+        HashMap<String,Integer>map=new HashMap<>();
+        map.put("max",0);
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]==arr[i-1]){
+                m++;
+            }else {
+                if(map.get("max")<m){
+                    map.put("max",m);
+                    x=arr[i-1];
+                    m=1;
+                }
+            }
+        }
+        if (map.get("max") < m) {
+            map.put("max", m);
+            x = arr[arr.length-1];
+        }
+        return x;
+    }
 }
--- src/LeetCode/T_1684.java
+++ src/LeetCode/T_1684.java
@@ -1,2 +1,32 @@
-package LeetCode;public class T_1684 {
+package LeetCode;
+
+import java.util.HashMap;
+
+public class T_1684 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int countConsistentStrings(String allowed, String[] words) {
+        int n = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < allowed.length(); i++) {
+            map.put(allowed.charAt(i), 1);
+        }
+        for (int i = 0; i < words.length; i++) {
+            boolean b=true;
+            for (int j = 0; j < words[i].length(); j++) {
+                if(!map.containsKey(words[i].charAt(j))){
+                    b=false;
+                    break;
+                }
+            }
+            if(b){
+                n++;
+            }
+        }
+        return n;
+    }
 }
