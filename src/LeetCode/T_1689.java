32e7252addf193e5fad3c8d3e3e7a554cfc3ac41
白云浩 <1152394813@qq.com>
2024-06-24 09:42:32 +0800

    2024-6-24

--- src/LeetCode/T_1689.java
+++ src/LeetCode/T_1689.java
@@ -1,2 +1,19 @@
-package LeetCode;public class T_1689 {
+package LeetCode;
+
+public class T_1689 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int minPartitions(String n) {
+        char c='0';
+        for (int i = 0; i < n.length(); i++) {
+            if(n.charAt(i)>c){
+                c=n.charAt(i);
+            }
+        }
+        return Integer.valueOf(String.valueOf(c));
+    }
 }
