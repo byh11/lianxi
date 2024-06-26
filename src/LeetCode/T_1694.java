d76ed66474754fe9ab156338d2187e8198646c3e
白云浩 <1152394813@qq.com>
2024-06-26 15:17:25 +0800

    2024-6-26

--- src/LeetCode/T_1694.java
+++ src/LeetCode/T_1694.java
@@ -1,2 +1,32 @@
-package LeetCode;public class T_1694 {
+package LeetCode;
+
+public class T_1694 {
+
+    public static void main(String[] args) {
+
+        reformatNumber("1-23-45 6");
+    }
+
+    public static String reformatNumber(String number) {
+        number=number.replace("-", "");
+        number=number.replace(" ","");
+        String s="";
+        for (int i = 0; i < number.length(); ) {
+            if(number.length()-i<=4){
+                if(number.length()-i==4){
+                    s+=number.substring(i,i+2);
+                    s+="-";
+                    s+=number.substring(i+2);
+                    break;
+                }else {
+                    s+=number.substring(i);
+                    break;
+                }
+            }else {
+                s+=number.substring(i,i+3)+"-";
+                i+=3;
+            }
+        }
+        return s;
+    }
 }
--- src/LeetCode/T_1716.java
+++ src/LeetCode/T_1716.java
@@ -1,2 +1,23 @@
-package LeetCode;public class T_1716 {
+package LeetCode;
+
+public class T_1716 {
+
+    public static void main(String[] args) {
+
+        totalMoney(8);
+    }
+
+    public static int totalMoney(int n) {
+        int sum=0;
+        int week=1,day=1;
+        for (int i = 0; i < n; i++) {
+            sum+=week+day-1;
+            if(day==7){
+                week++;
+                day=0;
+            }
+            day++;
+        }
+        return sum;
+    }
 }
