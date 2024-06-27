c66a4d97273afaac1ece07e5e75954f61b0f0beb
白云浩 <1152394813@qq.com>
2024-06-27 08:39:11 +0800

    2024-6-27

--- src/LeetCode/T_1732.java
+++ src/LeetCode/T_1732.java
@@ -1,2 +1,21 @@
-package LeetCode;public class T_1732 {
+package LeetCode;
+
+public class T_1732 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public int largestAltitude(int[] gain) {
+        int n=0;
+        int max=0;
+        for (int i = 0; i < gain.length; i++) {
+            n+=gain[i];
+            if(n>max){
+                max=n;
+            }
+        }
+        return max;
+    }
 }
--- src/LeetCode/T_1750.java
+++ src/LeetCode/T_1750.java
@@ -1,4 +1,26 @@
 package LeetCode;
 
+import java.util.ArrayList;
+
 public class T_1750 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int minimumLength(String s) {
+        int r=s.length()-1,l=0;
+        while (l<r && s.charAt(l)==s.charAt(r)){
+            char c1=s.charAt(l);
+            while (l<=r && s.charAt(l)==c1){
+                l++;
+            }
+            char c2=s.charAt(r);
+            while (l<=r && s.charAt(r)==c2){
+                r--;
+            }
+        }
+        return r-l+1;
+    }
 }
