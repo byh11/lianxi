34e0818aef4366ecf3bd7cdd0be546ce6120313f
白云浩 <1152394813@qq.com>
2024-07-02 08:34:13 +0800

    2024-7-2

---
+++ src/LeetCode/T_2710.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+public class T_2710 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public String removeTrailingZeros(String num) {
+        return num.replaceAll("0+$", ""); // È¥³ýÄ©Î²µÄ0
+
+    }
+}
