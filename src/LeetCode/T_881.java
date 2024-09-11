8b282a2b05056644e2167c6e5367c0bee54b16bb
白云浩 <1152394813@qq.com>
2024-09-11 15:20:29 +0800

    2024-9-11

--- src/LeetCode/T_881.java
+++ src/LeetCode/T_881.java
@@ -1,48 +1,27 @@
 package LeetCode;
 
 
-import java.util.*;
+import java.util.Arrays;
 
 public class T_881 {
-    //    public static int numRescueBoats(int[] people, int limit) {
-//        int n=0,x=0;
-//        List list=new ArrayList<>(Arrays.asList(people));
-//        for (int i = 0; i < people.length; i++) {
-//            list.add(people[i]);
-//        }
-//        list.sort(Comparator.reverseOrder());
-//        for (int i = 0; i < list.size(); i++) {
-//            if(list.get(i)<limit){
-//                for (int j = i+1; j < list.size(); j++) {
-//                    if ((list.get(i)+list.get(j)<=limit)){
-//                        n++;
-//                        list.remove(j);
-//                        list.remove(i);
-//                        i--;
-//                        x++;
-//                        break;
-//                    }
-//                }
-//                if(x==0){
-//                    n++;
-//                    list.remove(i);
-//                    i--;
-//                }
-//                x=0;
-//            }else {
-//                n++;
-//                list.remove(i);
-//                i--;
-//            }
-//        }
-//        return n;
-//    }
-//
-//    public static void main(String[] args) {
-//        System.out.println(numRescueBoats(new int[]{3, 8, 7, 1, 4}, 9));
-//    }
-//
-//    public static int numRescueBoats(int[] people, int limit) {
-//
-//    }
+    public static void main(String[] args) {
+
+    }
+
+    public static int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int l = 0, r = people.length - 1;
+        int a = 0;
+        while (l <= r) {
+            if (people[l] + people[r] <= limit) {
+                a++;
+                l++;
+                r--;
+            } else {
+                a++;
+                r--;
+            }
+        }
+        return a;
+    }
 }
